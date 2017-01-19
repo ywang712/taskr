7965d1c26e2ab4bf44fe56c743699924969c972b
Yong Wang <ywang712@gmail.com>
2017-01-18 23:25:32 -0500

    added task table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118132512) do
+ActiveRecord::Schema.define(version: 20170119041434) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
