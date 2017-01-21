4737de85b998170dfca0601a7f0c028eba808bfc
Yong Wang <ywang712@gmail.com>
2017-01-21 16:56:47 -0500

    added custom action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 	before_action :authenticate_user!
-	before_action :set_task, only:[:edit, :update, :show, :destroy]
+	before_action :set_task, only:[:edit, :update, :show, :destroy, :change]
 
 	def index
 		@to_do = current_user.tasks.where(state: 'to_do')
@@ -44,6 +44,13 @@ class TasksController < ApplicationController
 		
 		@task.destroy
 		flash[:notice] = "Task was successfully deleted"
+		redirect_to tasks_path
+	end
+
+
+	def change
+		@task.update_attributes(state: params[:state])
+		flash[:notice] = "Task status was successfully changed"
 		redirect_to tasks_path
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :tasks
+  resources :tasks do 
+  	member do
+  	put :change
+  end
+ end
   
   root 'pages#home'
 
